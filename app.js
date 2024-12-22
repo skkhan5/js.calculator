a4bb0e959400744eb9e0557396db08ca39e542f5
skkhan5 <yousafzai5588@gmail.com>
2024-12-23 02:16:11 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let a = +prompt("Enter First Number")
+let opratar = prompt("select oprator (+,-, /,*):")
+let b = +prompt("Enter Second Number")
+
+if( opratar == "+"){
+    document.write(a + b)
+}
+if( opratar == "-"){
+    document.write(a - b)
+}
+if( opratar == "*"){
+    document.write(a * b)
+}
+if( opratar == "/"){
+    document.write(a / b)
+}
